83a776c3808525947dedf32e66d1baf7179d2917
SkaMateria <skamateria@gmail.com>
2019-09-19 10:50:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: yellow')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener("DOMContentLoaded", function(){
+  
+  fetch(imgUrl)
+  .then(res => res.json())
+  .then(dogoUrls => {
+
+    dogoUrls.message.map(dogPicUrl => {
+      let frame = document.querySelector("#dog-image-container")
+      let dogElement = document.createElement("img")
+      dogElement.src = dogPicUrl
+      frame.appendChild(dogElement)
+    })
+  })
+
+  fetch(breedUrl)
+
+
+
+
+
+
+})
